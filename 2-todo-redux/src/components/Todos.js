2aeb4327c322641a30d02aa3f67df3436a67c98d
Allan Weber <a.cassianoweber@gmail.com>
2020-10-25 17:22:57 +0100

    Finish todo redux

--- 2-todo-redux/src/components/Todos.js
+++ 2-todo-redux/src/components/Todos.js
@@ -1,28 +1,28 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { fetchTasks, toggleCompleted } from '../redux/task/taskActions';
+import { fetchTasks, toggleCompleted, removeTodo } from '../redux/task/taskActions';
 
-const Todos = ({ tasks, fetchTasks, toggleCompleted }) => {
+const Todos = ({ tasks, fetchTasks, toggleCompleted, removeTodo }) => {
   useEffect(() => fetchTasks());
 
   const clickTask = (id) => {
     toggleCompleted(id);
   };
 
+  const removeTask = (id) => {
+    removeTodo(id);
+  };
+
   return (
     <div>
-      <h1>Todos</h1>
-      <div>
-        {tasks.map((todo) => (
-          <p
-            className={todo.completed ? 'line-through' : ''}
-            key={todo.id}
-            onClick={() => clickTask(todo.id)}
-            role="a">
-            {todo.name}
-          </p>
-        ))}
-      </div>
+      {tasks.map((todo) => (
+        <div className={todo.completed ? 'line-through' : ''} key={todo.id}>
+          <span onClick={() => clickTask(todo.id)}>{todo.name}</span>
+          <button type="button" onClick={() => removeTask(todo.id)}>
+            x
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
@@ -31,4 +31,4 @@ const mapStateToProps = (state) => ({
   tasks: state.tasks.items,
 });
 
-export default connect(mapStateToProps, { fetchTasks, toggleCompleted })(Todos);
+export default connect(mapStateToProps, { fetchTasks, toggleCompleted, removeTodo })(Todos);
